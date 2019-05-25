e1ac8a82adbe1c97139246bce87693d854e24841
Halil GÜLER <halilguler1994@gmail.com>
2019-05-26 01:05:57 +0300

    ErrorBoundary react 16 version adding project components error try catch

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 //import { robots } from "./robots/robots";
 import CardList from "../components/CardList";
+import ErrorBoundary from '../components/ErrorBoundary';
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
-import './App.css';
+import "./App.css";
+import ErrorBoundry from "../components/ErrorBoundary";
 class App extends Component {
   constructor() {
     super();
@@ -32,7 +34,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
