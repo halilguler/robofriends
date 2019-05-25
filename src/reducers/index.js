3814a118e512e2ec370d2be1bd7967209068aa9a
Halil GÜLER <halilguler1994@gmail.com>
2019-05-26 02:29:27 +0300

    Project redux Provider and createStore adding React-redux and redux import

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,7 @@ export const INITIAL_STATE = {
 export const searchRobots = (state = INITIAL_STATE, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
-      return Object.assign({}, state, action.payload);
+      return Object.assign({}, state, { searchField: action.payload });
     default:
       return state;
   }
